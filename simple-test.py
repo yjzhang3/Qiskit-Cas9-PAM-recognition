e64a28ed73260a66cad3050cb1416ba8578739b2
Yujia <56132313+yjzhang3@users.noreply.github.com>
2021-09-14 21:43:43 -0500

    Update simple-test.py

--- simple-test.py
+++ simple-test.py
@@ -1,3 +1,12 @@
+# This code is a part of Qiskit
+# © Copyright IBM 2017, 2021.
+
+# This code is licensed under the Apache License, Version 2.0. 
+
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 from qiskit import Aer
 
 from qiskit_nature.drivers import PySCFDriver, UnitsType, Molecule
